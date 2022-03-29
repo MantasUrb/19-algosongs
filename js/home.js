6ac0ccf9cbdb2448f76ae1fc2677c5c6ade306ee
Mantas Urbonas <mantasu11@gmail.com>
2022-03-29 21:23:46 +0300

    nav generated by js #1

--- js/home.js
+++ js/home.js
@@ -1,5 +1,46 @@
 // import JS modules
 
+import { nav } from './components/nav/nav.js'
+
 
 
 // execute JS modules
+
+// <!-- HEADER START -->
+nav('header nav')
+// <!-- HEADER END -->
+
+
+// <!-- HERO -->
+
+// <!-- HERO -->
+
+
+// <!-- SERVICES MUSIC PROMO SECTION START -->
+
+// <!-- SERVICES MUSIC PROMO SECTION END -->
+
+
+// <!-- SERVICES NFTS SECTION START -->
+
+// <!-- SERVICES NFTS SECTION END -->
+
+
+// <!-- SERVICES DISCORD MANAGEMENT SECTION START -->
+
+// <!-- SERVICES DISCORD MANAGEMENT SECTION END -->
+
+
+// <!-- ABOUT US SECTION START -->
+
+// <!-- ABOUT US SECTION END -->
+
+
+// <!-- PARTNERS SECTION START -->
+
+// <!-- PARTNERS SECTION END -->
+
+
+// <!-- FOOTER START -->
+
+// <!-- FOOTER END -->
