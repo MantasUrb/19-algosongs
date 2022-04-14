999e2b917a84b35225ad70226b5dd9e8ef0bf5d7
Mantas Urbonas <mantasu11@gmail.com>
2022-04-14 11:39:35 +0300

    blob size fix & click on mobile logo close menu #5

--- js/components/hamburger.js
+++ js/components/hamburger.js
@@ -2,18 +2,23 @@ function hamburger () {
 
     const hamburgerDOM = document.querySelector('.hamburger');
     const navDOM = document.querySelector('header nav');
+    const navLinksDOM = document.querySelectorAll('header nav a');
+    const headerlogoDOM = document.querySelector('.logo');
 
     hamburgerDOM.addEventListener('click', () => {
         navDOM.classList.toggle('hidden');
     })
-
-    const navLinksDOM = document.querySelectorAll('header nav a');
 
     for (const navLink of navLinksDOM) {
             navLink.addEventListener('click', () => {
                 navDOM.classList.add('hidden');
         })
     }
+
+    headerlogoDOM.addEventListener('click', () => {
+        navDOM.classList.add('hidden');
+    })
+    
 }
 
 export { hamburger }
