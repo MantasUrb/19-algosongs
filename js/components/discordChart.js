f946fa91c4bbdeca7c88b429cb653dcfdea85890
Mantas Urbonas <mantasu11@gmail.com>
2022-06-06 14:16:23 +0300

    animates on scroll (only desktop look good) #23

--- js/components/discordChart.js
+++ js/components/discordChart.js
@@ -8,11 +8,30 @@ function discordChart (selector, data) {
 
         HTML += `<div class="chart">
                     <div class="value">${column.value} millions</div>
-                    <div class="bar" style="height: ${pixelNumber}px"></div>
+                    <div class="progress" style="height: ${pixelNumber}px">
+                        <div class="bar"></div>
+                    </div>
                     <div class="year">${column.year}</div>
                 </div>`
     }
-    return DOM.innerHTML += HTML;
-}
+    DOM.innerHTML += HTML;
 
+    const discordChartsDOM = document.querySelectorAll('.chart');
+   
+    for (const discordChartDOM of discordChartsDOM) {
+
+        addEventListener('scroll', () => {
+
+            const elementTop = discordChartDOM.getBoundingClientRect().top;
+            const elementHeight = discordChartDOM.clientHeight;
+
+            const isVisible = innerHeight >= elementTop + elementHeight;
+        
+            if (isVisible) {
+                discordChartDOM.classList.add('animate');
+            }
+        })
+    }
+            
+}
 export { discordChart }
