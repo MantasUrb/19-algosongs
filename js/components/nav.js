7c0fcd4483f702289f4bdf0a916bd3c91aa5cf79
Mantas Urbonas <mantasu11@gmail.com>
2022-06-16 23:28:31 +0300

    js nav mobile menu fix

--- js/components/nav.js
+++ js/components/nav.js
@@ -1,10 +1,10 @@
 function nav (selector) {
 
     const formatTitle = (str) => {
-        if (str === "nfts") {
-            return "NFTs"
-        }
-        const parts = str.split('-');
+        const parts = str.split('_');
+        const firstWord = parts[0];
+        const firstUp = firstWord[0].toUpperCase() + firstWord.slice(1);
+        parts[0] = firstUp;
         return parts.join(' ');
     }
 
@@ -19,15 +19,16 @@ function nav (selector) {
     }
 
     const bodyChilderDOM = document.querySelectorAll('body > *');
-    // const ctaButton = `<a class="button" href='https://calendly.com/laurynas-brovka/discover' target='_blank'>contact</a>`
+    console.log(bodyChilderDOM);
+    let HTML = '';
 
     for (const sectionDOM of bodyChilderDOM) {
         if (sectionDOM.id) {
-            let HTML = `<a class="header-link" href="#${sectionDOM.id}">${formatTitle(sectionDOM.id)}</a>`;
-            DOM.insertAdjacentHTML('beforeend', HTML);
+            HTML += `<a class="header-link" href="#${sectionDOM.id}">${formatTitle(sectionDOM.id)}</a>`;
         } 
-    }   
-    return DOM.insertAdjacentHTML('beforeend', ctaButton);
+    }  
+
+    return DOM.innerHTML = HTML;
 }
 
 export { nav }
