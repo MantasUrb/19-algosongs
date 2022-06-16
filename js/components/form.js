65f53eae51f38d8f26d99413c83346a5914e3572
Mantas Urbonas <mantasu11@gmail.com>
2022-06-16 22:37:53 +0300

    footer looks good on mobile&desktop

--- js/components/form.js
+++ js/components/form.js
@@ -1,16 +1,5 @@
 function form () {
 
-    const inputFields = document.querySelectorAll('input, textarea');
-    const inputLabels = document.querySelectorAll('label');
-
-
-
-    // for (const inputField of inputFields) {
-    //     console.log(inputField.value);
-    //     if (!inputField.value.lenght == 0) {
-            
-    //     }
-    // }
 
 }
 
